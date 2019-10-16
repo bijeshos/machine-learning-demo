0d66e517c1a20e07d1d741d0cadb595c87c1cb95
Bijesh O S <bijeshos@users.noreply.github.com>
2019-10-16 22:34:34 +0530

    removed print statements

--- tensorflow/data-processing/data-processing.py
+++ tensorflow/data-processing/data-processing.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import functools
 
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 
 TRAIN_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/train.csv"
@@ -13,29 +14,32 @@ test_file_path = tf.keras.utils.get_file("eval.csv", TEST_DATA_URL)
 # Make numpy values easier to read.
 np.set_printoptions(precision=3, suppress=True)
 
-#!head {train_file_path}
+# !head {train_file_path}
 
 LABEL_COLUMN = 'survived'
 LABELS = [0, 1]
 
+
 def get_dataset(file_path, **kwargs):
-  dataset = tf.data.experimental.make_csv_dataset(
-      file_path,
-      batch_size=5, # Artificially small to make examples easier to show.
-      label_name=LABEL_COLUMN,
-      na_value="?",
-      num_epochs=1,
-      ignore_errors=True,
-      **kwargs)
-  return dataset
+    dataset = tf.data.experimental.make_csv_dataset(
+        file_path,
+        batch_size=5,  # Artificially small to make examples easier to show.
+        label_name=LABEL_COLUMN,
+        na_value="?",
+        num_epochs=1,
+        ignore_errors=True,
+        **kwargs)
+    return dataset
+
 
 raw_train_data = get_dataset(train_file_path)
 raw_test_data = get_dataset(test_file_path)
 
+
 def show_batch(dataset):
-  for batch, label in dataset.take(1):
-    for key, value in batch.items():
-      print("{:20s}: {}".format(key,value.numpy()))
+    for batch, label in dataset.take(1):
+        for key, value in batch.items():
+            print("{:20s}: {}".format(key, value.numpy()))
 
 
 show_batch(raw_train_data)
@@ -56,39 +60,43 @@ SELECT_COLUMNS = ['survived', 'age', 'n_siblings_spouses', 'parch', 'fare']
 DEFAULTS = [0, 0.0, 0.0, 0.0, 0.0]
 temp_dataset = get_dataset(train_file_path,
                            select_columns=SELECT_COLUMNS,
-                           column_defaults = DEFAULTS)
+                           column_defaults=DEFAULTS)
 
 show_batch(temp_dataset)
 
 example_batch, labels_batch = next(iter(temp_dataset))
 
+
 def pack(features, label):
-  return tf.stack(list(features.values()), axis=-1), label
+    return tf.stack(list(features.values()), axis=-1), label
+
 
 packed_dataset = temp_dataset.map(pack)
 
 for features, labels in packed_dataset.take(1):
-  print(features.numpy())
-  print()
-  print(labels.numpy())
+    print(features.numpy())
+    print()
+    print(labels.numpy())
 
 show_batch(raw_train_data)
 
 example_batch, labels_batch = next(iter(temp_dataset))
 
+
 class PackNumericFeatures(object):
-  def __init__(self, names):
-    self.names = names
+    def __init__(self, names):
+        self.names = names
 
-  def __call__(self, features, labels):
-    numeric_freatures = [features.pop(name) for name in self.names]
-    numeric_features = [tf.cast(feat, tf.float32) for feat in numeric_freatures]
-    numeric_features = tf.stack(numeric_features, axis=-1)
-    features['numeric'] = numeric_features
+    def __call__(self, features, labels):
+        numeric_freatures = [features.pop(name) for name in self.names]
+        numeric_features = [tf.cast(feat, tf.float32) for feat in numeric_freatures]
+        numeric_features = tf.stack(numeric_features, axis=-1)
+        features['numeric'] = numeric_features
 
-    return features, labels
+        return features, labels
 
-NUMERIC_FEATURES = ['age','n_siblings_spouses','parch', 'fare']
+
+NUMERIC_FEATURES = ['age', 'n_siblings_spouses', 'parch', 'fare']
 
 packed_train_data = raw_train_data.map(
     PackNumericFeatures(NUMERIC_FEATURES))
@@ -100,58 +108,59 @@ show_batch(packed_train_data)
 
 example_batch, labels_batch = next(iter(packed_train_data))
 
-import pandas as pd
 desc = pd.read_csv(train_file_path)[NUMERIC_FEATURES].describe()
 desc
 
 MEAN = np.array(desc.T['mean'])
 STD = np.array(desc.T['std'])
 
+
 def normalize_numeric_data(data, mean, std):
-  # Center the data
-  return (data-mean)/std
+    # Center the data
+    return (data - mean) / std
+
 
 # See what you just created.
 normalizer = functools.partial(normalize_numeric_data, mean=MEAN, std=STD)
 
 numeric_column = tf.feature_column.numeric_column('numeric', normalizer_fn=normalizer, shape=[len(NUMERIC_FEATURES)])
 numeric_columns = [numeric_column]
-numeric_column
+# numeric_column
 
-example_batch['numeric']
+# example_batch['numeric']
 
 numeric_layer = tf.keras.layers.DenseFeatures(numeric_columns)
 numeric_layer(example_batch).numpy()
 
 CATEGORIES = {
     'sex': ['male', 'female'],
-    'class' : ['First', 'Second', 'Third'],
-    'deck' : ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
-    'embark_town' : ['Cherbourg', 'Southhampton', 'Queenstown'],
-    'alone' : ['y', 'n']
+    'class': ['First', 'Second', 'Third'],
+    'deck': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+    'embark_town': ['Cherbourg', 'Southhampton', 'Queenstown'],
+    'alone': ['y', 'n']
 }
 
 categorical_columns = []
 for feature, vocab in CATEGORIES.items():
-  cat_col = tf.feature_column.categorical_column_with_vocabulary_list(
+    cat_col = tf.feature_column.categorical_column_with_vocabulary_list(
         key=feature, vocabulary_list=vocab)
-  categorical_columns.append(tf.feature_column.indicator_column(cat_col))
+    categorical_columns.append(tf.feature_column.indicator_column(cat_col))
 
-# See what you just created.
-categorical_columns
+# print categorical_columns
+# categorical_columns
 
 categorical_layer = tf.keras.layers.DenseFeatures(categorical_columns)
 print(categorical_layer(example_batch).numpy()[0])
 
-preprocessing_layer = tf.keras.layers.DenseFeatures(categorical_columns+numeric_columns)
+preprocessing_layer = tf.keras.layers.DenseFeatures(categorical_columns + numeric_columns)
 
 print(preprocessing_layer(example_batch).numpy()[0])
 
 model = tf.keras.Sequential([
-  preprocessing_layer,
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(1, activation='sigmoid'),
+    preprocessing_layer,
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid'),
 ])
 
 model.compile(
@@ -172,8 +181,6 @@ predictions = model.predict(test_data)
 
 # Show some results
 for prediction, survived in zip(predictions[:10], list(test_data)[0][1][:10]):
-  print("Predicted survival: {:.2%}".format(prediction[0]),
-        " | Actual outcome: ",
-        ("SURVIVED" if bool(survived) else "DIED"))
-
-
+    print("Predicted survival: {:.2%}".format(prediction[0]),
+          " | Actual outcome: ",
+          ("SURVIVED" if bool(survived) else "DIED"))
