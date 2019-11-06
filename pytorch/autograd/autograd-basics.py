8661118722887e9fb88d2e5eb0ab23479cc233d8
Bijesh O S <bijeshos@users.noreply.github.com>
2019-11-06 09:00:31 +0530

    added instructions to do back prop

--- pytorch/autograd/autograd-basics.py
+++ pytorch/autograd/autograd-basics.py
@@ -24,3 +24,27 @@ print(a.requires_grad)
 b = (a * a).sum()
 print(b.grad_fn)
 
+# gradients : back prop
+out.backward()
+print(x.grad)
+
+# vector-Jacobian product
+x = torch.randn(3, requires_grad=True)
+
+y = x * 2
+while y.data.norm() < 1000:
+    y = y * 2
+
+print(y)
+
+v = torch.tensor([0.1, 1.0, 0.0001], dtype=torch.float)
+y.backward(v)
+
+print(x.grad)
+
+# another variant
+print(x.requires_grad)
+print((x ** 2).requires_grad)
+
+with torch.no_grad():
+    print((x ** 2).requires_grad)
