2417e5af013ab641266866967a0fdf2f56ebaa4b
Bijesh O S <bijeshos@users.noreply.github.com>
2019-11-06 08:35:16 +0530

    pytorch: added various tensor operations

--- pytorch/basics/basics.py
+++ pytorch/basics/basics.py
@@ -37,3 +37,17 @@ print(result)
 
 y.add_(x)
 print(y)
+
+# indexing
+print(x[:, 1])
+
+# resizing
+x = torch.randn(4, 4)
+y = x.view(16)
+z = x.view(-1, 8)  # the size -1 is inferred from other dimensions
+print(x.size(), y.size(), z.size())
+
+# element retrieval
+x = torch.randn(1)
+print(x)
+print(x.item())
